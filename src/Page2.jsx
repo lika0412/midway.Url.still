671bf0a7231f3cef21c6f07a3fbfcc5d7e8fe48d
lika0412 <92198635+lika0412@users.noreply.github.com>
2021-11-12 22:04:05 +0900

    Initial commit

---
+++ src/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>2ページ目です</h1>
+    </div>
+  );
+};
---
+++ src/router/Router.jsx
@@ -0,0 +1,32 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { Page2 } from "../Page2";
+import { page1Routes } from "./Page1Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page1Routes.map((route) => (
+              <Route
+                key={route.Path}
+                exact={route.exact}
+                path={`${url}${route.Path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route path="/page2" />
+      <Page2 />
+    </Switch>
+  );
+};
